985ea9217b04fcf8a8f467269aaa74cb13b56e8a
Hubert Gee <hubert.gee@keysight.com>
2018-01-11 21:28:12 -0800

    First commit

---
+++ RestApi/Python/Modules/Utilities/IxNetCli/preference.py
@@ -0,0 +1,19 @@
+jsonConfigFile = 'bgpJsonOriginal.json'
+ixncfgConfigFile = 'bgp_ngpf_8.30.ixncfg'
+apiServerType = 'windows' ;# Use connecttolinux() or connecttowindows() to set this.
+windowsApiServerIp = '192.168.70.3'
+windowsApiServerIpPort = 11009
+chassisIp = '192.168.70.11'
+portList = [[chassisIp, 1,1], [chassisIp, 2,1]]
+linuxApiServerIp = '192.168.70.108'
+linuxApiServerIpPort = 443
+apiKey = 'eaebe82c72d541cfab384e911496b063'
+username = 'admin'
+password = 'admin'
+licenseServerIp = '192.168.70.3'
+licenseMode = 'subscription'
+licenseTier = 'tier3'
+forceTakePortOwnership = True
+deleteSessionAfterTest = True ;# For Linux only
+enableDebugTracing = False ;# Display error tracebacks
+

00efdc2961aba744a430a658ea2bd7eef71c86b0
Hubert Gee <hubert.gee@keysight.com>
2018-04-10 12:05:08 -0700

    First commit

---
+++ RestApi/Python/SampleScripts/mplsNgpf.py
@@ -0,0 +1,260 @@
+
+# PLEASE READ DISCLAIMER
+#
+#    This is a sample script for demo and reference purpose only.
+#    It is subject to change for content updates without warning.
+#
+# REQUIREMENTS
+#    - Python2.7 - 3.6
+#    - Python modules: requests
+#
+# DESCRIPTION
+#    This sample script demonstrates:
+#        - REST API configurations using two back-to-back Ixia ports.
+#        - Connecting to Windows IxNetwork API server or Linux API server.
+#
+#        - Verify for sufficient amount of port licenses before testing.
+#        - Verify port ownership.
+#        - Configure two IPv4/MPLS Topology Groups
+#        - Start protocols
+#        - Verify MPLS protocol sessions
+#        - Create a Traffic Item
+#        - Apply Traffic
+#        - Start Traffic
+#        - Get stats
+#
+# USAGE
+#    python <script>.py windows
+#    python <script>.py linux
+
+import sys, traceback
+
+sys.path.insert(0, '../Modules')
+from IxNetRestApi import *
+from IxNetRestApiPortMgmt import PortMgmt
+from IxNetRestApiTraffic import Traffic
+from IxNetRestApiProtocol import Protocol
+from IxNetRestApiStatistics import Statistics
+
+# Default the API server to either windows or linux.
+osPlatform = 'windows'
+
+if len(sys.argv) > 1:
+    if sys.argv[1] not in ['windows', 'windowsConnectionMgr', 'linux']:
+        sys.exit("\nError: %s is not a known option. Choices are 'windows' or 'linux'." % sys.argv[1])
+    osPlatform = sys.argv[1]
+
+try:
+    #---------- Preference Settings --------------
+
+    forceTakePortOwnership = True
+    releasePortsWhenDone = False
+    enableDebugTracing = True
+    deleteSessionAfterTest = False ;# For Windows Connection Mgr and Linux API server only
+
+    licenseIsInChassis = False
+    licenseServerIp = '192.168.70.3'
+    licenseModel = 'subscription'
+    licenseTier = 'tier3'
+
+    ixChassisIp = '192.168.70.11'
+    # [chassisIp, cardNumber, slotNumber]
+    portList = [[ixChassisIp, '1', '1'],
+                [ixChassisIp, '2', '1']]
+
+    if osPlatform == 'linux':
+        mainObj = Connect(apiServerIp='192.168.70.108',
+                          serverIpPort='443',
+                          username='admin',
+                          password='admin',
+                          deleteSessionAfterTest=deleteSessionAfterTest,
+                          verifySslCert=False,
+                          serverOs=osPlatform
+                          )
+
+    if osPlatform in ['windows', 'windowsConnectionMgr']:
+        mainObj = Connect(apiServerIp='192.168.70.3',
+                          serverIpPort='11009',
+                          serverOs=osPlatform,
+                          deleteSessionAfterTest=deleteSessionAfterTest,
+                          httpInsecure=True
+                          )
+
+    #---------- Preference Settings End --------------
+
+    portObj = PortMgmt(mainObj)
+    portObj.connectIxChassis(ixChassisIp)
+
+    if portObj.arePortsAvailable(portList, raiseException=False) != 0:
+        if forceTakePortOwnership == True:
+            portObj.releasePorts(portList)
+            portObj.clearPortOwnership(portList)
+        else:
+            raise IxNetRestApiException('Ports are owned by another user and forceTakePortOwnership is set to False')
+
+    mainObj.newBlankConfig()
+
+    # If the license is activated on the chassis's license server, this variable should be True.
+    # Otherwise, if the license is in a remote server or remote chassis, this variable should be False.
+    # Configuring license requires releasing all ports even for ports that is not used for this test.
+    if licenseIsInChassis == False:
+        portObj.releaseAllPorts()
+        mainObj.configLicenseServerDetails([licenseServerIp], licenseModel, licenseTier)
+
+    # Set createVports = True if building config from scratch.
+    portObj.assignPorts(portList, createVports=True)
+
+    protocolObj = Protocol(mainObj, portObj)
+    topologyObj1 = protocolObj.createTopologyNgpf(portList=[portList[0]],
+                                                  topologyName='Topo1')
+    
+    deviceGroupObj1 = protocolObj.createDeviceGroupNgpf(topologyObj1,
+                                                        multiplier=1,
+                                                        deviceGroupName='DG1')
+    
+    topologyObj2 = protocolObj.createTopologyNgpf(portList=[portList[1]],
+                                                  topologyName='Topo2')
+    
+    deviceGroupObj2 = protocolObj.createDeviceGroupNgpf(topologyObj2,
+                                                        multiplier=1,
+                                                        deviceGroupName='DG2')
+    
+    ethernetObj1 = protocolObj.createEthernetNgpf(deviceGroupObj1,
+                                                  ethernetName='MyEth1',
+                                                  macAddress={'start': '00:01:01:00:00:01',
+                                                              'direction': 'increment',
+                                                              'step': '00:00:00:00:00:01'},
+                                                  macAddressPortStep='disabled',
+                                                  vlanId={'start': 103,
+                                                          'direction': 'increment',
+                                                          'step':0})
+    
+    ethernetObj2 = protocolObj.createEthernetNgpf(deviceGroupObj2,
+                                                  ethernetName='MyEth2',
+                                                  macAddress={'start': '00:01:02:00:00:01',
+                                                              'direction': 'increment',
+                                                              'step': '00:00:00:00:00:01'},
+                                                  macAddressPortStep='disabled',
+                                                  vlanId={'start': 103,
+                                                          'direction': 'increment',
+                                                          'step':0})
+    
+    mplsObj1 = protocolObj.configMpls(ethernetObj=ethernetObj1,
+                                      name = 'mpls-1',
+                                      destMac = {'start': '00:01:02:00:00:01', 'direction': 'increment', 'step': '00:00:00:00:00:01'},
+                                      exp = {'start': 0, 'direction': 'increment', 'step': 1},
+                                      ttl = {'start': 16, 'direction': 'increment', 'step': 1},
+                                      rxLabelValue = {'start': 288, 'direction': 'increment', 'step': 1},
+                                      txLabelValue = {'start': 888, 'direction': 'increment', 'step': 1})
+
+    mplsObj2 = protocolObj.configMpls(ethernetObj=ethernetObj2,
+                                      name = 'mpls-2',
+                                      destMac = {'start': '00:01:01:00:00:01', direction': 'increment', 'step': '00:00:00:00:00:01'},
+                                      exp = {'start': 0, 'direction': 'increment', 'step': 1},
+                                      ttl = {'start': 16, 'direction': 'increment', 'step': 1},
+                                      rxLabelValue = {'start': 888, 'direction': 'increment', 'step': 1},
+                                      txLabelValue = {'start': 288, 'direction': 'increment', 'step': 1})
+
+    ipv4Obj1 = protocolObj.createIpv4Ngpf(ethernetObj1,
+                                          ipv4Address={'start': '1.1.1.1',
+                                                       'direction': 'increment',
+                                                       'step': '0.0.0.1'},
+                                          ipv4AddressPortStep='disabled',
+                                          gateway={'start': '1.1.1.2',
+                                                   'direction': 'increment',
+                                                   'step': '0.0.0.0'},
+                                          gatewayPortStep='disabled',
+                                          prefix=24,
+                                          resolveGateway=True)
+    
+    ipv4Obj2 = protocolObj.createIpv4Ngpf(ethernetObj2,
+                                          ipv4Address={'start': '1.1.1.2',
+                                                       'direction': 'increment',
+                                                       'step': '0.0.0.1'},
+                                          ipv4AddressPortStep='disabled',
+                                          gateway={'start': '1.1.1.1',
+                                                   'direction': 'increment',
+                                                   'step': '0.0.0.0'},
+                                          gatewayPortStep='disabled',
+                                          prefix=24,
+                                          resolveGateway=True)
+        
+    protocolObj.startAllProtocols()
+    protocolObj.verifyProtocolSessionsNgpf()
+
+    # For all parameter options, go to the API configTrafficItem.
+    # mode = create or modify
+    trafficObj = Traffic(mainObj)
+
+    trafficStatus = trafficObj.configTrafficItem(
+        mode='create',
+        trafficItem = {
+            'name':'Topo1 to Topo2',
+            'trafficType':'ipv4',
+            'biDirectional':True,
+            'srcDestMesh':'one-to-one',
+            'routeMesh':'oneToOne',
+            'allowSelfDestined':False,
+            'trackBy': ['flowGroup0', 'vlanVlanId0']},
+        endpoints = [{'name':'Flow-Group-1',
+                      'sources': [topologyObj1],
+                      'destinations': [topologyObj2]
+                  }],
+        configElements = [{'transmissionType': 'fixedFrameCount',
+                           'frameCount': 50000,
+                           'frameRate': 88,
+                           'frameRateType': 'percentLineRate',
+                           'frameSize': 128
+                       }])
+
+    trafficItemObj   = trafficStatus[0]
+    endpointObj      = trafficStatus[1][0]
+    configElementObj = trafficStatus[2][0]
+
+    trafficObj.regenerateTrafficItems()
+    trafficObj.startTraffic()
+
+    # Check the traffic state to assure traffic has stopped before checking for stats.
+    if trafficObj.getTransmissionType(configElementObj) == "fixedFrameCount":
+        trafficObj.checkTrafficState(expectedState=['stopped', 'stoppedWaitingForStats'], timeout=45)
+
+    statObj = Statistics(mainObj)
+    stats = statObj.getStats(viewName='Flow Statistics', silentMode=False)
+
+    print('\n{txPort:10} {txFrames:15} {rxPort:10} {rxFrames:15} {frameLoss:10}'.format(
+        txPort='txPort', txFrames='txFrames', rxPort='rxPort', rxFrames='rxFrames', frameLoss='frameLoss'))
+    print('-'*90)
+
+    for flowGroup,values in stats.items():
+        txPort = values['Tx Port']
+        rxPort = values['Rx Port']
+        txFrames = values['Tx Frames']
+        rxFrames = values['Rx Frames']
+        frameLoss = values['Frames Delta']
+
+        print('{txPort:10} {txFrames:15} {rxPort:10} {rxFrames:15} {frameLoss:10} '.format(
+            txPort=txPort, txFrames=txFrames, rxPort=rxPort, rxFrames=rxFrames, frameLoss=frameLoss))
+
+    if releasePortsWhenDone == True:
+        portObj.releasePorts(portList)
+
+    if osPlatform == 'linux':
+        mainObj.linuxServerStopAndDeleteSession()
+
+    if osPlatform == 'windowsConnectionMgr':
+        mainObj.deleteSession()
+
+except (IxNetRestApiException, Exception, KeyboardInterrupt) as errMsg:
+    if enableDebugTracing:
+        if not bool(re.search('ConnectionError', traceback.format_exc())):
+            print('\n%s' % traceback.format_exc())
+    print('\nException Error! %s\n' % errMsg)
+    if 'mainObj' in locals() and osPlatform == 'linux':
+        if deleteSessionAfterTest:
+            mainObj.linuxServerStopAndDeleteSession()
+    if 'mainObj' in locals() and osPlatform in ['windows', 'windowsConnectionMgr']:
+        if releasePortsWhenDone and forceTakePortOwnership:
+            portObj.releasePorts(portList)
+        if osPlatform == 'windowsConnectionMgr':
+            if deleteSessionAfterTest:
+                mainObj.deleteSession()

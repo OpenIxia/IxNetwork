e749a368e381c90e775cdea417d481045926a656
Hubert Gee <hubert.gee@keysight.com>
2018-05-05 13:18:02 -0700

    startTraffic to call regenerateTraffic

--- RestApi/Python/SampleScripts/addPacketHeaderRawTraffic.py
+++ RestApi/Python/SampleScripts/addPacketHeaderRawTraffic.py
@@ -244,8 +244,7 @@ try:
                                          })
     
     trafficObj.showTrafficItemPacketStack(configElementObj)
-    trafficObj.regenerateTrafficItems()
-    trafficObj.startTraffic()
+    trafficObj.startTraffic(regenerateTraffic=True, applyTraffic=True)
     
     # Check the traffic state to assure traffic has indeed stopped before checking for stats.
     if trafficObj.getTransmissionType(configElementObj) == "fixedFrameCount":

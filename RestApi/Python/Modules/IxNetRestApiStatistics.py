53ab042776e0c035a444665a1e25ffec713b67cf
Hubert Gee <hubert.gee@keysight.com>
2018-08-26 09:14:09 -0700

    Ehanced takeSnapshot to include Linux

--- RestApi/Python/Modules/IxNetRestApiStatistics.py
+++ RestApi/Python/Modules/IxNetRestApiStatistics.py
@@ -16,11 +16,13 @@ class Statistics(object):
         """
         self.ixnObj = mainObject
 
-    def getStats(self, viewObject=None, viewName='Flow Statistics', csvFile=None, csvEnableFileTimestamp=False, displayStats=True,
-                 silentMode=True, ignoreError=False):
+    def getStats(self, viewObject=None, viewName='Flow Statistics', csvFile=None, csvEnableFileTimestamp=False,
+                 displayStats=True, silentMode=False, ignoreError=False):
         """
         Description
             Get stats by the statistic name or get stats by providing a view object handle.
+            This API get stats in /api/v1/sessions/{id}/ixnetwork/statistics/view/{id}/data looking for
+            attributes columnCaptions, pageValues and totalPages.
 
         Parameters
             csvFile = None or <filename.csv>.
@@ -70,39 +72,62 @@ class Statistics(object):
            Get stats on row 2 for 'Tx Frames' = statDict[2]['Tx Frames']
         """
         if viewObject == None:
-            viewList = self.ixnObj.get('%s/%s/%s' % (self.ixnObj.sessionUrl, 'statistics', 'view'), silentMode=silentMode)
-            views = ['%s/%s/%s/%s' % (self.ixnObj.sessionUrl, 'statistics', 'view', str(i['id'])) for i in viewList.json()]
-            if silentMode is False:
-                self.ixnObj.logInfo('\ngetStats: Searching for viewObj for viewName: %s' % viewName)
-            for view in views:
-                #print('\nview:', view)
-                # GetAttribute
-                response = self.ixnObj.get('%s' % view, silentMode=silentMode)
-                captionMatch = re.match(viewName, response.json()['caption'], re.I)
-                if captionMatch:
-                    # viewObj: sessionUrl + /statistics/view/11'
-                    viewObject = view
+
+            breakFlag = 0
+            counterStop = 30
+            for counter in range(1, counterStop+1):
+                viewList = self.ixnObj.get('%s/%s/%s' % (self.ixnObj.sessionUrl, 'statistics', 'view'), silentMode=silentMode)
+                views = ['%s/%s/%s/%s' % (self.ixnObj.sessionUrl, 'statistics', 'view', str(i['id'])) for i in viewList.json()]
+                if silentMode is False:
+                    self.ixnObj.logInfo('\ngetStats: Searching for viewObj for viewName: {0}'.format(
+                        viewName), timestamp=False)
+
+                for view in views:
+                    #print('\nview:', view)
+                    response = self.ixnObj.get('%s' % view, silentMode=silentMode)
+                    captionMatch = re.match(viewName, response.json()['caption'], re.I)
+                    if captionMatch:
+                        # viewObj: sessionUrl + /statistics/view/11'
+                        viewObject = view
+                        breakFlag = 1
+                        break
+
+                if breakFlag == 1:
                     break
 
-            if viewObject == None and ignoreError == False:
-                raise IxNetRestApiException("viewObj wasn't found for viewName: %s" % viewName)
-            if viewObject == None and ignoreError == True:
-                return None
+                if counter < counterStop:
+                    self.ixnObj.logInfo('\nGetting statview [{0}] is not ready. Waiting {1}/{2} seconds.'.format(
+                        viewName, counter, counterStop), timestamp=False)
+                    time.sleep(1)
+                    continue
+                    
+                if counter == counterStop:
+                    if viewObject == None and ignoreError == False:
+                        raise IxNetRestApiException("viewObj wasn't found for viewName: {0}".format(viewName))
+
+                    if viewObject == None and ignoreError == True:
+                        return None
 
         if silentMode is False:
-            self.ixnObj.logInfo('\nviewObj: %s' % viewObject)
+            self.ixnObj.logInfo('\nviewObj is: %s' % viewObject)
 
-        for counter in range(0,31):
-            response = self.ixnObj.get(viewObject+'/page', silentMode=silentMode)
+        counterStop = 30
+        for counter in range(0, counterStop+1):
+            response = self.ixnObj.get(viewObject+'/data', silentMode=silentMode)
             totalPages = response.json()['totalPages']
-            if totalPages == 'null':
-                self.ixnObj.logInfo('\nGetting total pages is not ready yet. Waiting %d/30 seconds' % counter, timestamp=False)
+            #self.ixnObj.logInfo('totalPages: {0}'.format(totalPages), timestamp=False)
+
+            if totalPages == 'null' and counter < counterStop:
+                self.ixnObj.logInfo('\nGetting total pages is not ready yet. Waiting {0}/{1} seconds'.format(
+                    counter, counterStop), timestamp=False)
                 time.sleep(1)
-            if totalPages != 'null':
+                continue
+
+            if totalPages != 'null' and counter < counterStop:
                 break
-            if totalPages == 'null' and counter == 30:
-                self.ixnObj.logInfo('\ngetStats failed: Getting total pages')
-                return 1
+
+            if counter == counterStop:
+                raise IxNetRestApiException('getStats failed: Getting total pages')
 
         if csvFile != None:
             import csv
@@ -120,23 +145,43 @@ class Statistics(object):
             csvFile = open(csvFileName, 'w')
             csvWriteObj = csv.writer(csvFile)
 
-        # Get the stat column names
-        columnList = response.json()['columnCaptions']
-        if csvFile != None:
-            csvWriteObj.writerow(columnList)
-
         flowNumber = 1
         statDict = {}
-        # Get the stat values
+        getColumnCaptionFlag = 0
         for pageNumber in range(1,totalPages+1):
-            self.ixnObj.patch(viewObject+'/page', data={'currentPage': pageNumber}, silentMode=silentMode)
-            response = self.ixnObj.get(viewObject+'/page', silentMode=silentMode)
+            self.ixnObj.patch(viewObject+'/data', data={'currentPage': pageNumber}, silentMode=silentMode)
+
+            counterStop = 30
+            for counter in range(1, counterStop+1):
+                response = self.ixnObj.get(viewObject+'/data', silentMode=silentMode)
+                if counter < counterStop:
+                    if response.json()['columnCaptions'] == [] or response.json()['pageValues'] == []:
+                        self.ixnObj.logInfo('[{0}] stat values not ready yet. Wait {1}/{2} seconds.'.format(
+                            viewName, counter, counterStop))
+                        time.sleep(1)
+                        continue
+
+                    if response.json()['columnCaptions'] != [] or response.json()['pageValues'] != []:
+                        break
+
+                if counter == counterStop:
+                    raise IxNetRestApiException('IxNetwork API server failed to provide stats')
+
+            # Get the stat column names one time only
+            if getColumnCaptionFlag == 0:
+                getColumnCaptionFlag = 1
+                columnList = response.json()['columnCaptions']
+                if csvFile != None:
+                    csvWriteObj.writerow(columnList)
+                
             statValueList = response.json()['pageValues']
             for statValue in statValueList:
                 if csvFile != None:
                     csvWriteObj.writerow(statValue[0])
+
                 if displayStats:
                     self.ixnObj.logInfo('\nRow: %d' % flowNumber, timestamp=False)
+
                 statDict[flowNumber] = {}
                 index = 0
                 for statValue in statValue[0]:
@@ -203,43 +248,51 @@ class Statistics(object):
         """
         self.ixnObj.post(self.ixnObj.sessionUrl+'/operations/clearstats')
 
-    def takeSnapshot(self, viewName='Flow Statistics', windowsPath=None, localLinuxPath=None,
+    def takeSnapshot(self, viewName='Flow Statistics', windowsPath=None, isLinux=False, localLinuxPath=None,
                      renameDestinationFile=None, includeTimestamp=False, mode='overwrite'):
         """
         Description
             Take a snapshot of the vieweName statistics.  This is a two step process.
-            1> Take a snapshot of the statistics and store it in the C: drive.
-            2> Copy the statistics from the c: drive to remote Linux.
+            1> Take a snapshot of the statistics that you want and store it in the C: drive for Windows.
+               For Linux, the snapshot goes to /home/ixia_logs.
+            2> Copy the statistics from the snapshot locations to the local Linux where you ran the script..
 
         Parameters
             viewName: The name of the statistics to get.
-            windowsPath: A C: drive + path to store the snapshot: c:\\Results
-            localLinuxPath: None|A path. If None, this API won't copy the stat file to local Linux.
-                       The stat file will remain on Windows c: drive.
+            windowsPath: For Windows|WindowsConnectionMgr only.
+                         The C: drive + path to store the snapshot: Example: c:\\Results.
+            isLinux: <bool>: Defaults to False.  Set to True if you're getting the snapshot from Linux chassis.
+            localLinuxPath: None|path. Provide the local Linux path to put the snapshot file.
+                            If None, this API won't copy the stat file to local Linux.
+                            The stat file will remain on Windows c: drive.
             renameDestinationFile: None or a name of the file other than the viewName.
             includeTimestamp: True|False: To include a timestamp at the end of the file.
             mode: append|overwrite: append=To append stats to an existing stat file.
                                     overwrite=Don't append stats. Create a new stat file.
 
         Example:
-            takeSnapshot(viewName='Flow Statistics', windowsPath='C:\\Results', localLinuxPath='/home/hgee',
-                        renameDestinationFile='my_renamed_stat_file.csv', includeTimestamp=True)
+            For Windows:
+               statObj.takeSnapshot(viewName='Flow Statistics', windowsPath='C:\\Results', localLinuxPath='/home/hgee',
+                           renameDestinationFile='my_renamed_stat_file.csv', includeTimestamp=True)
+
+            For Linux:
+               statObj.takeSnapshot(viewName='Flow Statistics', isLinux=True, localLinuxPath='/home/hgee')
         """
-        # TODO: For Linux API server
-        #    POST /api/v1/sessions/1/ixnetwork/operations/getdefaultsnapshotsettings
-        #    "Snapshot.View.Csv.Location: \"/root/.local/share/Ixia/IxNetwork/data/logs/Snapshot CSV\""
-
-        if windowsPath is None:
-            raise IxNetRestApiException('\nMust include windowsPath\n')
-
         if mode == 'append':
             mode = 'kAppendCSVFile'
+
         if mode == 'overwrite':
             mode = 'kOverwriteCSVFile'
 
+        if windowsPath:
+            location = windowsPath
+
+        if isLinux:
+            location = '/home/ixia_logs'
+
         data = {'arg1': [viewName], 'arg2': [
                             "Snapshot.View.Contents: \"allPages\"",
-                            "Snapshot.View.Csv.Location: \"{0}\"".format(windowsPath),
+                            "Snapshot.View.Csv.Location: \"{0}\"".format(location),
                             "Snapshot.View.Csv.GeneratingMode: \"%s\"" % mode,
                             "Snapshot.View.Csv.StringQuotes: \"True\"",
                             "Snapshot.View.Csv.SupportsCSVSorting: \"False\"",
@@ -248,15 +301,19 @@ class Statistics(object):
                             "Snapshot.View.Csv.DecimalPrecision: \"3\""
                             ]
                 }
+
         url = self.ixnObj.sessionUrl+'/operations/takeviewcsvsnapshot'
         response = self.ixnObj.post(url, data=data)
         self.ixnObj.waitForComplete(response, url+'/'+response.json()['id'])
+        if isLinux:
+            snapshotFile = location + '/' + viewName + '.csv'
+            self.fileMgmtObj.copyFileLinuxToLocalLinux(linuxApiServerPathAndFileName=snapshotFile, localPath=localLinuxPath)
 
-        #response = self.ixnObj.get(self.ixnObj.sessionUrl+'/files?filename=Flow Statistics.csv&absolute=c:\\Results', ignoreError=True)
-        if localLinuxPath:
+        if windowsPath and localLinuxPath:
             # Get the snapshot. Use the csvFilename that was specified and the location
             self.fileMgmtObj.copyFileWindowsToLocalLinux('{0}\\{1}.csv'.format(windowsPath, viewName), localLinuxPath,
-                                                    renameDestinationFile=renameDestinationFile, includeTimestamp=includeTimestamp)
+                                                         renameDestinationFile=renameDestinationFile,
+                                                         includeTimestamp=includeTimestamp)
 
     def getViewObject(self, viewName='Flow Statistics'):
         """

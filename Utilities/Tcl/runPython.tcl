b8fdc1a0732b43564feddc8ca8a0919eb7c9530e
Hubert Gee <hubert.gee@keysight.com>
2018-09-12 19:25:02 -0700

    Initial commit

---
+++ Utilities/Tcl/runPython.tcl
@@ -0,0 +1,36 @@
+#!/bin/expect
+
+set pythonPath /usr/local/python3.7.0/bin/python3.7
+
+proc GetShellPrompt {} {
+   set prompt "(%|#|>|\\\$) ?$"
+   return $prompt
+}
+
+proc SendCommand { id command {timeout 120}} {
+    set timeout $timeout
+    #set send_human {.3 .4 0.6 .1 3}
+    puts "\nSendCommand: $command"
+
+    send -i $id "$command\r"
+    expect {
+        -i $id
+	-re [GetShellPrompt] {
+	}
+        timeout {
+            puts "Error: command timedout: $command"
+        }
+    }
+}
+
+spawn csh
+expect {
+    "$" {
+	puts "Connected"
+    }
+    timeout {
+	puts "Error: timeout"
+    }
+}
+
+SendCommand $spawn_id "$pythonPath ../../RestApi/Python/SampleScripts/bierRawTrafficOnly.py"

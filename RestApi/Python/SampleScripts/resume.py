a4f07edf178181f45fd5dd02515a9993582059d6
Hubert Gee <hubert.gee@keysight.com>
2018-10-07 18:54:03 -0700

    Demonstrate https support

--- RestApi/Python/SampleScripts/resume.py
+++ RestApi/Python/SampleScripts/resume.py
@@ -32,28 +32,41 @@ try:
     licenseModel = 'subscription'
     licenseTier = 'tier3'
 
-    ixChassisIp = '192.168.70.11'
+    ixChassisIp = '192.168.70.128'
     # [chassisIp, cardNumber, slotNumber]
     portList = [[ixChassisIp, '1', '1'],
-                [ixChassisIp, '2', '1']]
+                [ixChassisIp, '1', '2']]
 
     if osPlatform == 'linux':
-        mainObj = Connect(apiServerIp='192.168.70.121',
+        mainObj = Connect(apiServerIp='192.168.70.9',
                           serverIpPort='443',
                           username='admin',
                           password='admin',
                           deleteSessionAfterTest=deleteSessionAfterTest,
                           verifySslCert=False,
                           serverOs=osPlatform,
-                          apiKey='9277fc8fe92047f6a126f54481ba07fc',
-                          sessionId=1
+                          apiKey='173c9e239c714c8ea73d549c0e62cc82',
+                          sessionId=5
                           )
 
-    if osPlatform in ['windows', 'windowsConnectionMgr']:
+    if osPlatform in ['windows']:
         mainObj = Connect(apiServerIp='192.168.70.3',
                           serverIpPort='11009',
                           serverOs=osPlatform,
                           deleteSessionAfterTest=deleteSessionAfterTest
+                          )
+    
+    # Starting with version 8.50, Windows supports https.
+    # You are able to set your Windows API server to http, but if it's set to https,
+    # you need to set the serverIpPort and httpsSecured to True|False.
+    # Windows Connection Manager default serverIpPort for http (notSecured) = 8008.
+    if osPlatform in ['windowsConnectionMgr']:
+        mainObj = Connect(apiServerIp='192.168.70.3',
+                          serverIpPort='443',
+                          serverOs=osPlatform,
+                          deleteSessionAfterTest=deleteSessionAfterTest,
+                          sessionId=8021,
+                          httpsSecured=True
                           )
     
     trafficObj = Traffic(mainObj)

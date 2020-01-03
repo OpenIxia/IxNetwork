07f957371d2c6c053f229727cd48272f761c6aaa
Hubert Gee <hubert.gee@keysight.com>
2020-01-03 09:19:02 -0800

    Fixed logics and better descriptive comments

--- RestPy/SampleScripts/connectToExistingConfig.py
+++ RestPy/SampleScripts/connectToExistingConfig.py
@@ -2,20 +2,13 @@
 connectToExistingConfig.py
 
    Connecting to an existing session.
-      - For Windows, the default session ID=1.
-      - For Windows Connection Mgr, session ID=?
-      - For Linux, there are two ways to login:
-          1> Login with username/password
-          2> Use the API-Key instead.
-
-          - Then provide the Session ID to connect into.
 
 Supports IxNetwork API servers:
    - Windows, Windows Connection Mgr and Linux
 
 Requirements
    - IxNetwork 8.50
-   - RestPy version 1.0.33
+   - Minimum RestPy version 1.0.33
    - Python 2.7 and 3+
    - pip install requests
    - pip install -U --no-cache-dir ixnetwork_restpy
@@ -30,10 +23,10 @@ from ixnetwork_restpy.testplatform.testplatform import TestPlatform
 from ixnetwork_restpy.files import Files
 from ixnetwork_restpy.assistants.statistics.statviewassistant import StatViewAssistant
 
-apiServerIp = '192.168.70.3'
+apiServerIp = '192.168.70.12'
 
 # windows|connection_manager|linux
-osPlatform = 'windows'
+osPlatform = 'linux'
 
 # For Linux API server only
 username = 'admin'
@@ -46,19 +39,18 @@ try:
     testPlatform.Trace = 'request_response'
 
     if osPlatform == 'linux':
-        # There are two ways to log into an existing linux session ID
+        # There are two ways to log into an existing linux session
 
         # 1> Provide the login account username and password
         testPlatform.Authenticate(username, password)
 
-        # Two ways to locate a session to connecto. Either by the session ID or by a name that you could name.
-        #session = testPlatform.Sessions.find(Id=4)
-        session = testPlatform.Sessions.find(Name='devTest')
-
-        # 2> Or use the API-Key instead. The API-Key could be retrieve from the Linux api server under 
-        #    settings, My Account.
+        # 2> Or use the API-Key instead. 
+        # The API-Key could be retrieve from the Linux api server under settings, My Account.
         #testPlatform.ApiKey = '604348999bc34d028043347f713e49ce'
-        #session = testPlatform.Sessions.find(Id=4)
+
+        # After logging in, there are two ways to connect to a specific session.
+        session = testPlatform.Sessions.find(Id=2)
+        #session = testPlatform.Sessions.find(Name='devTest')
 
     if osPlatform in ['windows', 'connection_manager']:
         # Windows support only one session. Id is always equal 1.

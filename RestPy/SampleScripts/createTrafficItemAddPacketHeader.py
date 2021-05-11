9417856d998530824209f42f3450e6eb87322cca
Hubert Gee <hubogee@gmail.com>
2021-05-11 13:31:31 -0700

    Use StackTypeId instead of DisplayName

--- RestPy/SampleScripts/createTrafficItemAddPacketHeader.py
+++ RestPy/SampleScripts/createTrafficItemAddPacketHeader.py
@@ -27,11 +27,11 @@ from pprint import pprint
 # Import the RestPy module
 from ixnetwork_restpy import SessionAssistant
 
-apiServerIp = '192.168.70.3'
+apiServerIp = '192.168.129.6'
 
 # A list of chassis to use instead of the ones in the loaded config file
-ixChassisIpList = ['192.168.70.128']
-portList = [[ixChassisIpList[0], 1, 1], [ixChassisIpList[0], 2, 1]]
+ixChassisIpList = ['192.168.129.15']
+portList = [[ixChassisIpList[0], 1, 1], [ixChassisIpList[0], 1, 2]]
 
 # For Linux API server only
 username = 'admin'
@@ -50,21 +50,21 @@ try:
         # Do the followings to add packet headers on the new traffic item
 
         # Uncomment this to show a list of all the available protocol templates to create (packet headers)
-        #for protocolHeader in ixNetwork.Traffic.ProtocolTemplate.find():
-        #    ixNetwork.info('Protocol header: -- {} --'.format(protocolHeader.DisplayName))
+        # for protocolHeader in ixNetwork.Traffic.ProtocolTemplate.find():
+        #     ixNetwork.info('Protocol header: --{}--'.format(protocolHeader.StackTypeId))
 
         # 1> Get the <new packet header> protocol template from the ProtocolTemplate list.
-        packetHeaderProtocolTemplate = ixNetwork.Traffic.ProtocolTemplate.find(DisplayName=packetHeaderToAdd)
+        packetHeaderProtocolTemplate = ixNetwork.Traffic.ProtocolTemplate.find(StackTypeId=packetHeaderToAdd)
         ixNetwork.info('protocolTemplate: {}'.format(packetHeaderProtocolTemplate))
 
         # 2> Append the <new packet header> object after the specified packet header stack.
-        appendToStackObj = configElement.Stack.find(DisplayName=appendToStack)
+        appendToStackObj = configElement.Stack.find(StackTypeId=appendToStack)
         ixNetwork.info('appendToStackObj: {}'.format(appendToStackObj))
         appendToStackObj.Append(Arg2=packetHeaderProtocolTemplate)
 
         # 3> Get the new packet header stack to use it for appending an IPv4 stack after it.
         # Look for the packet header object and stack ID.
-        packetHeaderStackObj = configElement.Stack.find(DisplayName=packetHeaderToAdd)
+        packetHeaderStackObj = configElement.Stack.find(StackTypeId=packetHeaderToAdd)
         
         # 4> In order to modify the fields, get the field object
         packetHeaderFieldObj = packetHeaderStackObj.Field.find()
@@ -105,25 +105,25 @@ try:
   
     # The Ethernet packet header doesn't need to be created.
     # It is there by default. Just do a find for the Ethernet stack object.
-    ethernetStackObj = ixNetwork.Traffic.TrafficItem.find(Name='Raw packet').ConfigElement.find()[0].Stack.find(DisplayName='Ethernet II')
+    ethernetStackObj = ixNetwork.Traffic.TrafficItem.find(Name='Raw packet').ConfigElement.find()[0].Stack.find(StackTypeId='ethernet$')
 
     # NOTE: If you are using virtual ports (IxVM), you must use the Destination MAC address of 
     #       the IxVM port from your virtual host (ESX-i host or KVM)
     ixNetwork.info('Configuring Ethernet packet header')
     ethernetDstField = ethernetStackObj.Field.find(DisplayName='Destination MAC Address')
     ethernetDstField.ValueType = 'increment'
-    ethernetDstField.StartValue = "00:0c:29:3a:8a:3a"
+    ethernetDstField.StartValue = "00:0c:29:68:05:1E"
     ethernetDstField.StepValue = "00:00:00:00:00:00"
     ethernetDstField.CountValue = 1
 
     ethernetSrcField = ethernetStackObj.Field.find(DisplayName='Source MAC Address')
     ethernetSrcField.ValueType = 'increment'
-    ethernetSrcField.StartValue = "00:0c:29:86:ba:0e"
+    ethernetSrcField.StartValue = "00:0c:29:68:05:14"
     ethernetSrcField.StepValue = "00:00:00:00:00:00"
     ethernetSrcField.CountValue = 1
 
     # VLAN
-    vlanFieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='^VLAN', appendToStack='Ethernet II')
+    vlanFieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='^vlan$', appendToStack='ethernet$')
     vlanIdField = vlanFieldObj.find(DisplayName='VLAN-ID')
     vlanIdField.SingleValue = 103
         
@@ -137,7 +137,7 @@ try:
     pfcQueueObj.ValueList = [1, 3, 5, 7]
 
     # PFC PAUSE: PFC PAUSE (802.1Qbb)
-    pauseFrameObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='^PFC PAUSE \(802.1Qbb\)', appendToStack='Ethernet II')
+    pauseFrameObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='pfcPause', appendToStack='ethernet$')
     pauseFrameField = pauseFrameObj.find(DisplayName='Control opcode')
     pauseFrameField.ValueType = 'singleValue'
     pauseFrameField.SingleValue = 103
@@ -155,7 +155,7 @@ try:
 
 
     # IPv4
-    ipv4FieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='IPv4', appendToStack='^VLAN')
+    ipv4FieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='ipv4', appendToStack='^vlan$')
     ipv4SrcField = ipv4FieldObj.find(DisplayName='Source Address')
     ipv4SrcField.ValueType = 'increment'
     ipv4SrcField.StartValue = '1.1.1.1'
@@ -216,7 +216,7 @@ try:
     #ipv4DefaultPHBField.CountValue = 9
 
     # Example to show appending UDP after the IPv4 header
-    udpFieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='^UDP', appendToStack='IPv4')
+    udpFieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='^udp$', appendToStack='ipv4')
     udpSrcField = udpFieldObj.find(DisplayName='UDP-Source-Port')
     udpSrcField.Auto = False
     udpSrcField.SingleValue = 1000
@@ -226,7 +226,7 @@ try:
     udpDstField.SingleValue = 1001
 
     # Example to show appending TCP after the IPv4 header
-    tcpFieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='^TCP', appendToStack='IPv4')
+    tcpFieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='tcp', appendToStack='ipv4')
     tcpSrcField = tcpFieldObj.find(DisplayName='TCP-Source-Port')
     tcpSrcField.Auto = False
     tcpSrcField.ValueType = 'valueList'
@@ -237,7 +237,7 @@ try:
     tcpDstField.SingleValue = 1003
 
     # Example to show appending ICMP after the IPv4 header
-    icmpFieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='ICMP Msg Type: 9', appendToStack='IPv4')
+    icmpFieldObj = createPacketHeader(rawTrafficItemObj, packetHeaderToAdd='icmpv9', appendToStack='ipv4')
 
 
     # Optional: Enable tracking to track your packet headers:

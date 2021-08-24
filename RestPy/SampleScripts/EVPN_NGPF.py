b1c5f4bce23156cd9a378cdd8ac2ddf19e85969d
Hubert Gee <hubogee@gmail.com>
2021-08-24 13:58:21 -0700

    Minor cleanup

--- RestPy/SampleScripts/EVPN_NGPF.py
+++ RestPy/SampleScripts/EVPN_NGPF.py
@@ -1,6 +1,8 @@
 """
 Evpn_NGPF.py:
+
    Tested with two back-2-back Ixia ports and put variables for the VTEP scale (L2/L3 VNI, number of VTEPs, ETC)
+
    - Connect to the API server
    - Assign ports:
         - If variable forceTakePortOwnership is True, take over the ports if they're owned by another user.
@@ -13,56 +15,55 @@ Evpn_NGPF.py:
    - Start traffic
    - Get Traffic Item
    - Get Flow Statistics stats
+
 Supports IxNetwork API servers:
    - Windows, Windows Connection Mgr and Linux
+
 Requirements:
    - Minimum IxNetwork 8.52
    - Python 2.7 and 3+
    - pip install requests
    - pip install ixnetwork_restpy
+
 RestPy Doc:
     https://www.openixia.github.io/ixnetwork_restpy/#/
+
 Usage:
    - Enter: python <script>
 """
 
-
 import sys, os, time, traceback
-
 from ixnetwork_restpy import SessionAssistant
 
-apiServerIp = '192.168.186.131'
+apiServerIp = '172.16.101.3'
 
-ixChassisIpList = ['192.168.186.129']
-portList = [[ixChassisIpList[0], 1,1], [ixChassisIpList[0], 2, 1]]
+ixChassisIpList = ['172.16.102.5']
+portList = [[ixChassisIpList[0], 1,1], [ixChassisIpList[0], 1, 2]]
 
 # For Linux API server only
 username = 'admin'
 password = 'admin'
 
-
 # For linux and connection_manager only. Set to True to leave the session alive for debugging.
-debugMode = True
-
+debugMode = False
 forceTakePortOwnership = True
 
+vtepMultiplier = '64'
 
-vtep_multiplier = '64'
+l2LabelStartValue = '1001'
+l3LabelStartValue = '1001001'
 
-l2_label_start_value = '1001'
-l3_label_start_value = '1001001'
+# eviCount has to be evenly divisable by l3RtCount for the script to work.
+eviCount = '128'
+l3RtCount = 8
 
-# evi_count has to be evenly divisable by l3_rt_count for the script to work.
-evi_count = '128'
-l3_rt_count = 8
-
-l3_rt_repeat_value = (int(evi_count) / l3_rt_count)
+l3RtRepeatValue = (int(eviCount) / l3RtCount)
 
 try:
     # LogLevel: none, info, warning, request, request_response, all
     session = SessionAssistant(IpAddress=apiServerIp, RestPort=None, UserName=username, Password=password,
                                SessionName=None, SessionId=None, ApiKey=None,
-                               ClearConfig=True, LogLevel='info', LogFilename='restpy.log')
+                               ClearConfig=True, LogLevel='all', LogFilename='restpy.log')
 
     ixNetwork = session.Ixnetwork
     
@@ -77,12 +78,9 @@ try:
 
     ixNetwork.info('Creating Topology Group 1')
     topology1 = ixNetwork.Topology.add(Name='Topo1', Ports=vport['Port_1'])
-    deviceGroup1 = topology1.DeviceGroup.add(Name='DG1', Multiplier=vtep_multiplier)
+    deviceGroup1 = topology1.DeviceGroup.add(Name='DG1', Multiplier=vtepMultiplier)
     ethernet1 = deviceGroup1.Ethernet.add(Name='Eth1')
     ethernet1.Mac.Increment(start_value='00:01:01:01:00:01', step_value='00:00:00:00:00:01')
-    # ethernet1.EnableVlans.Single(True)
-    # ixNetwork.info('Configuring vlanID')
-    # vlanObj = ethernet1.Vlan.find()[0].VlanId.Increment(start_value=103, step_value=0)
 
     ixNetwork.info('Configuring IPv4')
     ipv4 = ethernet1.Ipv4.add(Name='Ipv4')
@@ -98,13 +96,13 @@ try:
     bgp1.FilterEvpn.Single(True)
 
     ixNetwork.info('Configuring EVPN 1')
-    evpn1 = bgp1.BgpIPv4EvpnVXLAN.add(Name= 'EVPN_VXLAN_1')
-    bgp1.BgpEthernetSegmentV4.EvisCount = evi_count
+    evpn1 = bgp1.BgpIPv4EvpnVXLAN.add(Name='EVPN_VXLAN_1')
+    bgp1.BgpEthernetSegmentV4.EvisCount = eviCount
     bgp1.BgpEthernetSegmentV4.VtepIpv4Address.Increment(start_value='10.1.1.1', step_value='0.0.0.1')
     evpn1.NumBroadcastDomainV4 = '1' 
-    evpn1.RdEvi.Custom(start_value='1', step_value='0',increments=[('1', l3_rt_count, [('0', l3_rt_repeat_value , [])])])
-    evpn1.BgpL3VNIExportRouteTargetList.find()[0].TargetAssignedNumber.Custom(start_value=l3_label_start_value, step_value='0',increments=[('1', l3_rt_count, [('0', l3_rt_repeat_value , [])])])
-    evpn1.BgpExportRouteTargetList.find()[0].TargetAssignedNumber.Custom(start_value=l2_label_start_value, step_value='0',increments=[('1', evi_count, [])])
+    evpn1.RdEvi.Custom(start_value='1', step_value='0',increments=[('1', l3RtCount, [('0', l3RtRepeatValue , [])])])
+    evpn1.BgpL3VNIExportRouteTargetList.find()[0].TargetAssignedNumber.Custom(start_value=l3LabelStartValue, step_value='0',increments=[('1', l3RtCount, [('0', l3RtRepeatValue , [])])])
+    evpn1.BgpExportRouteTargetList.find()[0].TargetAssignedNumber.Custom(start_value=l2LabelStartValue, step_value='0',increments=[('1', eviCount, [])])
 
     ixNetwork.info('Configuring Network Group 1')
     networkGroup1 = deviceGroup1.NetworkGroup.add(Name='BGP-Routes1', Multiplier='1')
@@ -115,21 +113,18 @@ try:
     ipv4PrefixPool = macPool.Ipv4PrefixPools.add(NumberOfAddresses='1')
     ipv4PrefixPool.NetworkAddress.Increment(start_value='20.20.0.1', step_value='0.0.1.0')
     ipv4PrefixPool.PrefixLength.Single(16)
-    macPool.CMacProperties.find()[0].FirstLabelStart.Custom(start_value=l2_label_start_value, step_value='0',increments=[('1', evi_count, [])])
+    macPool.CMacProperties.find()[0].FirstLabelStart.Custom(start_value=l2LabelStartValue, step_value='0',increments=[('1', eviCount, [])])
     macPool.CMacProperties.find()[0].EnableSecondLabel.Single (True)
-    macPool.CMacProperties.find()[0].SecondLabelStart.Custom(start_value=l3_label_start_value, step_value='0',increments=[('1', l3_rt_count, [('0', l3_rt_repeat_value , [])])])
+    macPool.CMacProperties.find()[0].SecondLabelStart.Custom(start_value=l3LabelStartValue, step_value='0',increments=[('1', l3RtCount, [('0', l3RtRepeatValue , [])])])
     macPool.CMacProperties.find()[0].AdvertiseIpv4Address.Single (True)
     macPool.CMacProperties.find()[0].Ipv4AddressPrefixLength.Single(32)
 
     ixNetwork.info('Creating Topology Group 2')
     topology2 = ixNetwork.Topology.add(Name='Topo2', Ports=vport['Port_2'])
-    deviceGroup2 = topology2.DeviceGroup.add(Name='DG2', Multiplier=vtep_multiplier)
+    deviceGroup2 = topology2.DeviceGroup.add(Name='DG2', Multiplier=vtepMultiplier)
 
     ethernet2 = deviceGroup2.Ethernet.add(Name='Eth2')
     ethernet2.Mac.Increment(start_value='00:01:01:02:00:01', step_value='00:00:00:00:00:01')
-    # ethernet2.EnableVlans.Single(True)
-    # ixNetwork.info('Configuring vlanID')
-    # vlanObj = ethernet2.Vlan.find()[0].VlanId.Increment(start_value=103, step_value=0)
 
     ixNetwork.info('Configuring IPv4 2')
     ipv4 = ethernet2.Ipv4.add(Name='Ipv4-2')
@@ -146,12 +141,12 @@ try:
 
     ixNetwork.info('Configuring EVPN 2')
     evpn2 = bgp2.BgpIPv4EvpnVXLAN.add(Name= 'EVPN_VXLAN_2')
-    bgp2.BgpEthernetSegmentV4.EvisCount = evi_count
+    bgp2.BgpEthernetSegmentV4.EvisCount = eviCount
     bgp2.BgpEthernetSegmentV4.VtepIpv4Address.Increment(start_value='10.1.2.1', step_value='0.0.0.1')
     evpn2.NumBroadcastDomainV4 = '1' 
-    evpn2.RdEvi.Custom(start_value='1', step_value='0',increments=[('1', l3_rt_count, [('0', l3_rt_repeat_value , [])])])
-    evpn2.BgpL3VNIExportRouteTargetList.find()[0].TargetAssignedNumber.Custom(start_value=l3_label_start_value, step_value='0',increments=[('1', l3_rt_count, [('0', l3_rt_repeat_value , [])])])
-    evpn2.BgpExportRouteTargetList.find()[0].TargetAssignedNumber.Custom(start_value=l2_label_start_value, step_value='0',increments=[('1', evi_count, [])])
+    evpn2.RdEvi.Custom(start_value='1', step_value='0',increments=[('1', l3RtCount, [('0', l3RtRepeatValue , [])])])
+    evpn2.BgpL3VNIExportRouteTargetList.find()[0].TargetAssignedNumber.Custom(start_value=l3LabelStartValue, step_value='0',increments=[('1', l3RtCount, [('0', l3RtRepeatValue , [])])])
+    evpn2.BgpExportRouteTargetList.find()[0].TargetAssignedNumber.Custom(start_value=l2LabelStartValue, step_value='0',increments=[('1', eviCount, [])])
 
     ixNetwork.info('Configuring Network Group 2')
     networkGroup2=deviceGroup2.NetworkGroup.add(Name='BGP-Routes-2', Multiplier='1')
@@ -163,12 +158,11 @@ try:
     ipv4PrefixPool = macPool2.Ipv4PrefixPools.add(NumberOfAddresses='1')
     ipv4PrefixPool.NetworkAddress.Increment(start_value='20.20.0.2', step_value='0.0.1.0')
     ipv4PrefixPool.PrefixLength.Single(16)
-    macPool2.CMacProperties.find()[0].FirstLabelStart.Custom(start_value=l2_label_start_value, step_value='0',increments=[('1', evi_count, [])])
+    macPool2.CMacProperties.find()[0].FirstLabelStart.Custom(start_value=l2LabelStartValue, step_value='0',increments=[('1', eviCount, [])])
     macPool2.CMacProperties.find()[0].EnableSecondLabel.Single (True)
-    macPool2.CMacProperties.find()[0].SecondLabelStart.Custom(start_value=l3_label_start_value, step_value='0',increments=[('1', l3_rt_count, [('0', l3_rt_repeat_value , [])])])
+    macPool2.CMacProperties.find()[0].SecondLabelStart.Custom(start_value=l3LabelStartValue, step_value='0',increments=[('1', l3RtCount, [('0', l3RtRepeatValue , [])])])
     macPool2.CMacProperties.find()[0].AdvertiseIpv4Address.Single (True)
     macPool2.CMacProperties.find()[0].Ipv4AddressPrefixLength.Single(32)
-
 
     ixNetwork.StartAllProtocols(Arg1='sync')
 
